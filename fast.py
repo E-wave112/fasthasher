aedbcb72425d1daac6ee84ecbf62634427da1d2a
E-wave112 <osagieiyayi09@gmail.com>
2020-12-13 21:42:55 +0000

    added constraints to the input data

--- fast.py
+++ fast.py
@@ -4,7 +4,9 @@ import hashlib
 app = FastAPI()
 
 @app.get('/')
-async def root(password: str):
+async def root(password):
+    ##specify constraints
+    if len(password) < 6:
+        return {'small':'password must be at least 6 characters'}
     ##hash data integrity using the powerful and fast blake hash algorithm
     return {"protected password":hashlib.blake2b(password.encode()).hexdigest()}
-
