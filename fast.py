95b6355184a8b58e503e7dcbaea8288ca52d8a3b
E-wave112 <osagieiyayi09@gmail.com>
2021-02-01 20:49:10 +0000

    implemented the password generator functionality

--- fast.py
+++ fast.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import random
 import hashlib
 
 app = FastAPI()
@@ -10,3 +11,14 @@ async def root(password):
         return {'small':'password must be at least 6 characters'}
     ##hash data integrity using the powerful and fast blake hash algorithm
     return {"protected password":hashlib.blake2b(password.encode()).hexdigest()}
+
+##route for generating strong passwords
+@app.get('/generate')
+async def generate():
+    pass_list = [random.randint(65,90), random.randint(65,90), random.randint(65,90),random.randint(97,122),
+        random.randint(65,90), random.randint(65,90), random.randint(65,90),random.randint(97,122),
+        random.randint(65,90),random.randint(65,90),random.randint(97,122),random.randint(65,122)]
+    return {"generated_password":''.join([chr(i) for i in pass_list])}
+    
+    
+
