c43cbec45f98dfc7682c55a731b41d066190bc98
E-wave112 <osagieiyayi09@gmail.com>
2021-02-12 22:41:29 +0000

    added a little docs to my code

--- fast.py
+++ fast.py
@@ -2,19 +2,37 @@ from fastapi import FastAPI
 import random
 import hashlib
 
-app = FastAPI()
 
-@app.get('/')
+tags_metadata = [
+    {
+        "name": "hash",
+        "description": "Accepts a password parameter in string format to be hashed",
+    },
+    {
+        "name": "generate",
+        "description": "On request generates a 12 random character password"
+        
+    },
+]
+app = FastAPI(
+
+ title="My Py Hashgen",
+    description="A starter code on building python hashers and generators with fastApi",
+    version="1.0.0",
+    openapi_tags=tags_metadata
+)
+
+@app.get('/',tags=['hash'])
 ##function to hash a password
 async def root(password):
     ##specify constraints
     if len(password) < 6:
-        return {'small':'password must be at least 6 characters'}
+        return {'short':'password must be at least 6 characters'}
     ##hash data integrity using the powerful and fast blake hash algorithm
     return {"protected password":hashlib.blake2b(password.encode()).hexdigest()}
 
 ##route for generating strong passwords
-@app.get('/generate')
+@app.get('/generate',tags=['generate'])
 ##function to gewnerate a strong passowrd
 async def generate():
     pass_list = [random.randint(65,90), random.randint(65,90), random.randint(65,90),random.randint(97,122),
